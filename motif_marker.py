087376e111e5cf37353f49a32a98ea8148501bb7
Dillon Brownell <dillonbrownell214@gmail.com>
2022-02-15 14:45:20 -0800

    degenerate_motifs

--- motif_marker.py
+++ motif_marker.py
@@ -14,6 +14,69 @@ fasta = args.f
 motif = args.m
 
 fasta_file = open(fasta, "r")
+motif_file = open(motif, "r")
 
-for line in fasta_file:
-    print(line)
+
+seq_lines = fasta_file.readlines()
+motif_lines = motif_file.readlines()
+
+degendict = {
+    "W":"[A,T]",
+    "S":"[C,G]",
+    "M":"[A,C]",
+    "K":"[G,T]",
+    "P":"[A,G]",
+    "B":"[G,C,T]",
+    "D":"[A,G,T]",
+    "H":"[A,C,T]",
+    "V":"[A,G,C]",
+    "N":"[A,G,C,T]"
+}
+
+
+
+def translatedegenerate(string):
+    
+
+
+
+class seq:
+    def __init__(self, name, sequence):
+        self.name = name
+        self.seq = sequence
+        self.searchpattern = 
+
+
+
+
+class motif:
+    def __init__(self, motif):
+        self.motif = motif
+    def searcher(self, seqobject):
+        
+
+linecounter = 0
+linenames = []
+lineseqs = []
+making_lineseq = ""
+
+for line in seq_lines:
+    if line[0] == ">":
+        if linecounter != 0:
+            lineseqs.append(making_lineseq.strip())
+            making_lineseq = ""
+        linenames.append(line)
+        linecounter+=1
+        continue
+    
+    making_lineseq += line.strip()
+lineseqs.append(making_lineseq)
+
+seq_objects = [seq(linenames[num], lineseqs[num]) for num in range(linecounter)]
+
+motif_objects = [motif(line.strip()) for line in motif_lines]
+
+
+print(motif_objects[0].motif)
+
+
