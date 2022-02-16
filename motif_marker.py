80dc399065baf2c9f2b83ec38db3c7926e973f9e
Dillon Brownell <dillonbrownell214@gmail.com>
2022-02-16 00:28:44 -0800

    added motif regex

--- motif_marker.py
+++ motif_marker.py
@@ -26,17 +26,21 @@ degendict = {
     "M":"[A,C]",
     "K":"[G,T]",
     "P":"[A,G]",
+    "Y":"[C,T]",
     "B":"[G,C,T]",
     "D":"[A,G,T]",
     "H":"[A,C,T]",
     "V":"[A,G,C]",
-    "N":"[A,G,C,T]"
+    "N":"[A,G,C,T]",
+    "A":"A",
+    "C":"C",
+    "T":"T",
+    "G":"G",
+    "U":"U"
 }
 
-
-
 def translatedegenerate(string):
-    
+    return "".join([degendict[x] for x in string])
 
 
 
@@ -44,15 +48,23 @@ class seq:
     def __init__(self, name, sequence):
         self.name = name
         self.seq = sequence
-        self.searchpattern = 
-
-
+        self.associated_motifs = {}
+    
+    def find_motif_occurences(self, motif_object_name, motif_search_seq):
+        p = re.compile(motif_search_seq)
+        for m in p.finditer((self.seq).upper()):
+            print(m.start(), m.group())
+        
+        
+        
+        
 
 
 class motif:
     def __init__(self, motif):
         self.motif = motif
-    def searcher(self, seqobject):
+        self.searchpattern = translatedegenerate(self.motif.upper())
+    
         
 
 linecounter = 0
@@ -76,7 +88,7 @@ seq_objects = [seq(linenames[num], lineseqs[num]) for num in range(linecounter)]
 
 motif_objects = [motif(line.strip()) for line in motif_lines]
 
-
-print(motif_objects[0].motif)
+print(motif_objects[1].motif)
+seq_objects[2].find_motif_occurences(motif_objects[1].motif, motif_objects[1].searchpattern)
 
 
